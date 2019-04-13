dd565264bcfaeb9fdae1b56653869c1748ab28af
Tomasz Guz <tomasz.guz@gmail.com>
2019-04-13 19:04:05 +0200

    removes generation of syntax css

--- site.hs
+++ site.hs
@@ -2,19 +2,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
 import           Hakyll
-import           Text.Pandoc.Highlighting
 
 --------------------------------------------------------------------------------
 
-syntaxHighlightingStyle :: Style
-syntaxHighlightingStyle = pygments
-
 main :: IO ()
 main = hakyll $ do
-
-    preprocess $ do
-        let css = styleToCss syntaxHighlightStyle
-        writeFile "css/syntax.css" css >> putStrLn "  Generated css\\syntax.css"
 
     match "images/*" $ do
         route   idRoute
